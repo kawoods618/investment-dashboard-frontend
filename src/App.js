7ee833e2f4754968b2c49fb211df78d4568c0deb
kawoods618 <kawoods618@gmail.com>
2025-03-20 18:13:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import TradingViewWidget from "./TradingViewWidget";
+import TradingViewWidget from "./components/TradingViewWidget";
 
 const App = () => {
     const [ticker, setTicker] = useState("");
