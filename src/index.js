8234f69843f3413ca1bc6ec58044c2572de2283a
kawoods618 <kawoods618@gmail.com>
2025-03-22 09:11:41 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./App.css"; // ✅ FIXED
+import "./App.css"; // ✅ Correct file
 
 const container = document.getElementById("root");
 const root = createRoot(container);
